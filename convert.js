58aab889724fb3032a896e04be5a9a8858efe69d
tmo-taka <45462276+tmo-taka@users.noreply.github.com>
2022-08-03 14:54:42 +0900

    Feature/test (#1)
    * うまいことリサイズされないかったものに絶対値を入れて対応
    
    * ちょっと簡略的に描いて見た
    
    * 一旦リサイズして書き出す関数を作り、サイズはmap化した
    
    * もう少しやって見た
    
    * sharp trimをぶち込んでみた
    
    * ピアノにも対応

--- convert.js
+++ convert.js
@@ -5,12 +5,25 @@ import {isNotJunk} from 'junk';
 import {pdfToPng} from 'pdf-to-png-converter';
 // 「process.argv[2]」に、引数として指定する変換対象ファイル名が渡される。
 
-async function main(type,number) {
+// サイズ表のマップ
+var sizeMap = new Map
+sizeMap.set('normal',{width: 60, height:30});
+sizeMap.set('small',{width: 50, height:25})
+sizeMap.set('big',{width: 120, height:60})
+
+async function main(type,number,trim=false) {
     console.log(type,number)
     const logoDirectory = './img/logo' 
     let files = fs.readdirSync(logoDirectory)
     let file = files.filter(isNotJunk)[0]
     let extend = file.split(".").pop();
+    console.log(file)
+    if(trim){
+        await sharp(logoDirectory+ '/' + file)
+            .trim()
+            .toFile(logoDirectory+ '/' + `tirm.${extend}`);
+        file=`tirm.${extend}`;
+    }
 
     if(extend === "pdf"){
         //NOTE: PDFの場合はPNGに変換する
@@ -27,26 +40,43 @@ async function main(type,number) {
     // ファイル読み込み
     if(type == "HIKKOSHI"){
         // TODO: 拡張子対応しなければ
-        const image = await jimp.read(logoDirectory + '/' + file);
-        const image_2 = await jimp.read(logoDirectory + '/' + file);
+        const image = await createOriginLogo(2,logoDirectory,file)
 
-        const image_normal = await logo_resize(image,[60,30]);
-        const image_small = await logo_resize(image_2,[50,25]);
-
-        // TODO: 拡張子対応しなければ
-        await image_normal.writeAsync('img/image_normal.' + extend);
-        await image_small.writeAsync('img/image_small.'+ extend);
-
-        // 背景の余白を作成
-        const backImage_normal = createBackImage(60,30)
-        const backImage_small = createBackImage(50,25)
+        const normal = await resize_writen(image[0],'normal',extend)
+        const small = await resize_writen(image[1],'small',extend)
 
         //TODO: 画像の長さを見て、長さに合わせて、どちらをautoにするか決める必要あり
-        await putLogo(backImage_normal,image_normal);
-        await putLogo(backImage_small,image_small);
+        await putLogo(normal.back,normal.image);
+        await putLogo(small.back,small.image);
 
-        await outputLogo(backImage_normal,'normal')
-        await outputLogo(backImage_small,'small')
+        await outputLogo(normal.back,'normal',type)
+        await outputLogo(small.back,'small',type)
+    }
+
+    else if(type == "PIANO"){
+        // TODO: 拡張子対応しなければ
+        const image = await createOriginLogo(3,logoDirectory,file)
+
+        const normal = await resize_writen(image[0],'normal',extend)
+        const small = await resize_writen(image[1],'small',extend)
+        const big = await resize_writen(image[2],'big',extend)
+
+        //TODO: 画像の長さを見て、長さに合わせて、どちらをautoにするか決める必要あり
+        await putLogo(normal.back,normal.image);
+        await putLogo(small.back,small.image);
+        await putLogo(big.back,big.image);
+
+        await outputLogo(normal.back,'normal',type)
+        await outputLogo(small.back,'small',type)
+        await outputLogo(big.back,'big',type)
+    }
+
+async function createOriginLogo(number=2,logoDirectory,file) {
+    const img = [];
+    for (let i=0; i<number; i++ ){
+        img[i] = await jimp.read(logoDirectory + '/' + file)
+    }
+    return img;
 }
 
 function createBackImage(x, y) {
@@ -55,14 +85,27 @@ function createBackImage(x, y) {
     });
 }
 
+async function resize_writen(logo,key,extend) {
+    const {width,height} = sizeMap.get(key);
+    //対象画像
+    const image = await logo_resize(logo,{width,height});
+    //背景作成
+    const back = await createBackImage(width,height)
+    await image.writeAsync(`img/image_${key}` +'.' + extend);
+    return {image, back};
+}
+
 async function logo_resize(logo,size){
-    const width = logo.bitmap.width
-    const height = logo.bitmap.height
-    if(width > height){
-        const image = await logo.resize(size[0], jimp.AUTO);
+    const {width, height} = size
+    const logoWidth = logo.bitmap.width
+    const logoHeight = logo.bitmap.height
+    const widthAbsolute = Math.abs(logoWidth- width);
+    const heighAbsolute = Math.abs(logoHeight- height);
+    if(widthAbsolute > heighAbsolute){
+        const image = await logo.resize(width, jimp.AUTO);
         return image
     }else {
-        const image = await logo.resize(jimp.AUTO, size[1]);
+        const image = await logo.resize(jimp.AUTO, height);
         return image
     }
 }
@@ -70,6 +113,7 @@ async function logo_resize(logo,size){
 async function putLogo(back,logo){
     const height = logo.bitmap.height
     const backHeight = back.bitmap.height
+
     if(height == backHeight){
         const centerPosion = (back.bitmap.width - logo.bitmap.width) / 2;
         await back.blit(logo,centerPosion,0);
@@ -79,14 +123,27 @@ async function putLogo(back,logo){
     }
 }
 
-async function outputLogo(back,type){
+async function outputLogo(back,size,type){
     var name
-    switch(type){
-        case 'small':
-            name = 'logo_s';
-            break;
-        default:
-            name = 'logo';
+    if(type === "HIKKOSHI"){
+        switch(size){
+            case 'small':
+                name = 'logo_s';
+                break;
+            default:
+                name = 'logo';
+        }
+    }else if(type === "PIANO"){
+        switch(size){
+            case 'small':
+                name = 'logo_ss';
+                break;
+            case 'normal':
+                name = 'logo_s';
+                break;
+            default:
+                name = 'logo';
+        }
     }
     await back.writeAsync('img/' + number + '_' + name +'.png')
     await sharp('img/' + number + '_' + name +'.png')
@@ -97,7 +154,10 @@ async function outputLogo(back,type){
 
 // NOTE: 引数取得
 const args = process.argv.slice(2)
+console.log(args)
 // NOTE: 引数を関数に渡す
-if(args[0] && args[1]){
+if(args[0] && args[1] && args[2]){
+    main(args[0],args[1],args[2]);
+}else if(args[0] && args[1] && !args[2]){
     main(args[0],args[1]);
 }
